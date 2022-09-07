40dcde414161f7aa75430547c3159f4835e9a63b
jonatas <santosjonaras@gmail.com>
2022-09-07 19:15:37 -0300

    first commit

---
+++ src/data/word.js
@@ -0,0 +1,10 @@
+
+
+export const wordsList ={
+  carro:["motor","porta","capo","pneu","antena"],
+  fruta:["banana","maça","pera","mamão","laranja"],
+  corpo:["braço","perna","cerebro","pescoço","olhos"],
+  computador:["mouse","teclado","monitor","gabinete"],
+progrmação:["linguagem", "framework","javascript","react"]
+,alimento:["arroz","feijão","carne","leite","ovo"]
+}
